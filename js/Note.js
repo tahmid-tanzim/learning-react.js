f89eb44c5d96de62804602550ed03996240747d4
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-08 16:52:13 +0600

    react using State

--- js/Note.js
+++ js/Note.js
@@ -5,21 +5,34 @@ var Note = React.createClass({
     remove: function () {
         alert("Removing Note");
     },
+    getInitialState: function () {
+        return {checked: true};
+    },
+    handleCheck: function () {
+        this.setState({checked: !this.state.checked});
+    },
     render: function () {
         return (
-            <div className="note">
-                <p>{this.props.children}</p>
-                <span>
-                    <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
-                    <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
-                </span>
+            <div>
+                <div className="note">
+                    <p>{this.props.children}</p>
+                    <span>
+                        <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
+                        <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
+                    </span>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked}/> This
+                        box is {this.state.checked ? "checked" : "unchecked"}
+                    </label>
+                </div>
             </div>
         );
     }
 });
 
-React.render(<Note>Hello World</Note>,
-    document.getElementById('react-container'));
+React.render(<Note>Hello World</Note>, document.getElementById('react-container'));
 
 
 
