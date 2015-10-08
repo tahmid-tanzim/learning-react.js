ea74bd703861efce69a15502ef720d2f941bf08a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-08 16:08:23 +0600

    React Handling Events

--- build/app.js
+++
@@ -1,6 +0,0 @@
-var MyComponent = React.createClass({displayName: "MyComponent",
-    render: function() {
-        return React.createElement("div", null, "My React Component");
-    }
-});
-React.render(React.createElement("div", null, React.createElement(MyComponent, null), React.createElement(MyComponent, null), React.createElement(MyComponent, null)), document.getElementById('content'));
--- src/app.js
+++
@@ -1,6 +0,0 @@
-var MyComponent = React.createClass({
-    render: function() {
-        return <div>My React Component</div>;
-    }
-});
-React.render(<div><MyComponent/><MyComponent/><MyComponent/></div>, document.getElementById('content'));
