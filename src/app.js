7667e7d33349deb423044f1fadee52af972bc955
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-10-08 15:20:19 +0600

    Creating a React component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-var MyContainer = React.createClass({
+var MyComponent = React.createClass({
     render: function() {
-        return (
-            <div>
-            <h2>{this.props.text}</h2>
-        <p>{this.props.children}</p>
-        </div>
-        );
+        return <div>My React Component</div>;
     }
 });
-React.render(<div>
-                <MyContainer text="Hello World">This is a Hello.</MyContainer>
-                <MyContainer text="How are you?">This is a how are you?</MyContainer>
-                <MyContainer text="Good Bye">This is a good bye.</MyContainer>
-    </div>, document.getElementById('content'));
+React.render(<div><MyComponent/><MyComponent/><MyComponent/></div>, document.getElementById('content'));
